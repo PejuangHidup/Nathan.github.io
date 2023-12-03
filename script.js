008caabeab23829419af555e1fddc8b569059746
PejuangHidup <152766850+PejuangHidup@users.noreply.github.com>
2023-12-03 13:27:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const wrapper document.querySelector(".wrapper");
+
+const question document.querySelector(".question"); const gif document.querySelector(".gif");
+
+const yesBtn = document.querySelector(".yes-btn");
+
+const noBtn document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", (){
+
+question, innerHTML = "Aaaaa, I like you too";
+
+"https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp"; });
+
+gif.src =
+
+noßtn.addEventListener("mouseover", () {
+
+const noßtnRect noßtn.getBoundingClientRect();
+
+const maxX = window.innerWidth noßtnRect.width;
+
+const maxy = window.innerHeight noBtnRect.height;
+
+const randomX Math.floor(Math.random() maxx);
+
+const randomy = Math.floor(Math.random() maxY);
+
+noßtn.style.left noßtn.style.top }); randomX+ "px"; randomy + "px";
